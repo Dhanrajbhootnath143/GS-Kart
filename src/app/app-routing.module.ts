46ddb9c29c752df005c8f9cea1937ee90a2010fa
Dhanrajbhootnath143 <dkumar60933@gmail.com>
2023-02-06 14:20:42 +0530

    loginpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminloginComponent } from './admin/adminlogin/adminlogin.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'adminlogin',component: AdminloginComponent},
+  
+  {
+  path: 'adminhome', component: AdminloginComponent,
+    children: []
+    },
+    
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
